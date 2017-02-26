8e047214889798fdb2cf1ab84701b3a5cf034d2b
Florent Pepin <florentpepin.92@gmail.com>
2017-02-26 14:51:48 +0000

    supersimplestocks: add passing dividend_yield method for Stock class

--- supersimplestocks.py
+++ supersimplestocks.py
@@ -1,17 +1,24 @@
+import decimal
+
 class Stock(object):
     ''' This class represents a common stock. '''
 
     def __init__(self, symbol, last_dividend, par_value):
         self.symbol = symbol
-        self.last_dividend = last_dividend
-        self.par_value = par_value
+        self.last_dividend = decimal.Decimal(last_dividend)
+        self.par_value = decimal.Decimal(par_value)
         self.trades = []
 
     def dividend_yield(self, market_price):
-        pass
+        if market_price <= 0:
+            raise ValueError("market price value is negative or equal to 0. Found {}".format(market_price))
+        return self.last_dividend / decimal.Decimal(market_price)
 
 class PreferredStock(Stock):
     ''' This class represents a preferred stock. '''
     def __init__(self, symbol, last_dividend, par_value, fixed_dividend):
         super(PreferredStock, self).__init__(symbol, last_dividend, par_value)
-        self.fixed_dividend = fixed_dividend
+        self.fixed_dividend = decimal.Decimal(fixed_dividend)
+
+    def dividend_yield(self, market_price):
+        pass
